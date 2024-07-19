29d0387e878b94b11d1808116b98b7058224d1fe
Viren070 <viren070@protonmail.com>
2024-07-19 22:13:08 +0100

    feat: use thread locking to avoid issue and catch error when data is not path

--- src/settings/cache.py
+++ src/settings/cache.py
@@ -2,10 +2,11 @@ import json
 import os
 import shutil
 import time
-
+from threading import RLock
 
 class Cache:
     def __init__(self, master, settings, metadata):
+        self.lock = RLock()
         self.master = master
         self.settings = settings
         self.metadata = metadata
@@ -22,101 +23,120 @@ class Cache:
             self.create_index_file()
 
     def is_index_file_valid(self):
-        if not os.path.exists(self.index_file):
-            return False
-        with open(self.index_file, "r", encoding="utf-8") as file:
-            try:
-                contents = json.load(file)
-                if contents.get("cache_version") != self.current_cache_version:
-                    return False
-            except json.JSONDecodeError:
+        with self.lock:
+            if not os.path.exists(self.index_file):
                 return False
-        return True
+            with open(self.index_file, "r", encoding="utf-8") as file:
+                try:
+                    contents = json.load(file)
+                    if contents.get("cache_version") != self.current_cache_version:
+                        return False
+                except json.JSONDecodeError:
+                    return False
+            return True
 
     def create_index_file(self):
-        os.makedirs(os.path.dirname(self.index_file), exist_ok=True)
-        with open(self.index_file, "w", encoding="utf-8") as file:
-            json.dump({"cache_version": 2}, file, indent=4)
+        with self.lock:
+            os.makedirs(os.path.dirname(self.index_file), exist_ok=True)
+            with open(self.index_file, "w", encoding="utf-8") as file:
+                json.dump({"cache_version": 2}, file, indent=4)
 
     def get_data_from_cache(self, key):
-        if not self.is_index_file_valid():
-            self.create_index_file()
-            return None
-        with open(self.index_file, "r", encoding="utf-8") as file:
-            index = json.load(file)
-        data = index.get(key)
-        if data is None:
-            return None
-        if not os.path.isfile(data["data"]):
-            self.remove_from_index(key)
-            return None
-        return data
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
+                return None
+            with open(self.index_file, "r", encoding="utf-8") as file:
+                index = json.load(file)
+            data = index.get(key)
+            if data is None:
+                return None
+            try:
+                if not os.path.isfile(data["data"]):
+                    self.remove_from_index(key)
+                    return None
+            except TypeError:
+                pass 
+            
+            return data
 
     def add_to_index(self, key, data):
-        if not self.is_index_file_valid():
-            self.create_index_file()
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
 
-        with open(self.index_file, "r", encoding="utf-8") as file:
-            index = json.load(file)
+            with open(self.index_file, "r", encoding="utf-8") as file:
+                index = json.load(file)
 
-        index[key] = {
-            "data": data,
-            "time": time.time()
-        }
-        try:
-            with open(self.index_file, "w", encoding="utf-8") as file:
-                json.dump(index, file, indent=4)
-        except Exception as error:
-            return (False, error)
-        return (True, data)
+            index[key] = {
+                "data": data,
+                "time": time.time()
+            }
+            try:
+                with open(self.index_file, "w", encoding="utf-8") as file:
+                    json.dump(index, file, indent=4)
+            except Exception as error:
+                return (False, error)
+            return (True, data)
 
     def remove_from_index(self, key, delete_data=True):
-        if not self.is_index_file_valid():
-            self.create_index_file()
-            return None
-        with open(self.index_file, "r", encoding="utf-8") as file:
-            index = json.load(file)
-        if key in index:
-            if os.path.isfile(str(index[key]["data"])) and delete_data:
-                os.remove(index[key]["data"])
-            del index[key]
-        with open(self.index_file, "w", encoding="utf-8") as file:
-            json.dump(index, file, indent=4)
+        print(f"Removing {key} from cache with delete_data={delete_data}")
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
+                return None
+            with open(self.index_file, "r", encoding="utf-8") as file:
+                index = json.load(file)
+            if key in index:
+                try:
+                    if os.path.isfile(str(index[key]["data"])) and delete_data:
+                        print(f"Removing {index[key]['data']} from cache folder")
+                        os.remove(index[key]["data"])
+                except OSError: 
+                    pass
+                del index[key]
+            with open(self.index_file, "w", encoding="utf-8") as file:
+                json.dump(index, file, indent=4)
 
     def add_json_data_to_cache(self, key, data):
-        if not self.is_index_file_valid():
-            self.create_index_file()
-        # create a new file with the data
-        path = os.path.join(self.cache_directory, "files", f"{key}.json")
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        # write the data to the file
-        with open(path, "w", encoding="utf-8") as file:
-            json.dump(data, file, indent=4)
-        # add the path to the index file
-        self.add_to_index(key, path)
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
+            # create a new file with the data
+            path = os.path.join(self.cache_directory, "files", f"{key}.json")
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            # write the data to the file
+            with open(path, "w", encoding="utf-8") as file:
+                json.dump(data, file, indent=4)
+            # add the path to the index file
+            self.add_to_index(key, path)
 
     def get_json_data_from_cache(self, key):
-        if not self.is_index_file_valid():
-            self.create_index_file()
-            return None
-        # get the path from the index file
-        data = self.get_data_from_cache(key)
-        if data is None:
-            return None
-        # read the data from the file
-        with open(data["data"], "r", encoding="utf-8") as file:
-            return {"data": json.load(file), "time": data["time"]}
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
+                return None
+            # get the path from the index file
+            data = self.get_data_from_cache(key)
+            if data is None:
+                return None
+            # read the data from the file
+            with open(data["data"], "r", encoding="utf-8") as file:
+                return {"data": json.load(file), "time": data["time"]}
 
     def add_custom_file_to_cache(self, key, file_path):
-        if not self.is_index_file_valid():
-            self.create_index_file()
-        # declare variable to store the path of the new file
-        new_dir = os.path.join(self.cache_directory, "files")
-        os.makedirs(new_dir, exist_ok=True)
-        path = os.path.join(new_dir, os.path.basename(file_path))
-        try:
-            shutil.move(file_path, path)
-        except OSError as error:
-            return (False, error)
-        # add this path to the index file with given key
-        return self.add_to_index(key, path)
+        with self.lock:
+            if not self.is_index_file_valid():
+                self.create_index_file()
+            # declare variable to store the path of the new file
+            new_dir = os.path.join(self.cache_directory, "files")
+            os.makedirs(new_dir, exist_ok=True)
+            path = os.path.join(new_dir, os.path.basename(file_path))
+            try:
+                shutil.move(file_path, path)
+            except OSError as error:
+                return (False, error)
+            # add this path to the index file with given key
+            return self.add_to_index(key, path)
+
+        
